854a5e488058eb86b7ad71b539f2edb0423ae49e
David Kiki <david.kariuki2@student.moringaschool.com>
2025-05-19 16:13:22 +0300

    Completed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,10 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    def parse(self):
+        parts = re.split(r'[,\s]+', self.addresses.strip())
+        valid_emails = [email for email in parts if re.fullmatch(r'\b[\w\.-]+@[\w\.-]+\.\w+\b', email)]
+        return sorted(set(valid_emails))
